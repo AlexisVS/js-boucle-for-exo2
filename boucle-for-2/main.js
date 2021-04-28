892623df921e64290fe6bc0c96ff03614eb23110
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-04-28 20:40:20 +0200

    Add files via upload

---
+++ boucle-for-2/main.js
@@ -0,0 +1,17 @@
+let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+eleves.forEach(e => {
+    console.log("bonjour " + e + "\n");
+});
+
+let arr = [1, 2, 3, 4, 5];
+arr.forEach(f => {
+    console.log(f)
+});
+
+
+
+let tableau = ["a","b","c","d","e","f"];
+tableau.forEach( e => {
+    console.log( "yo" + e );
+    
+});
